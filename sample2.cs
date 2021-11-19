528b80445fb96180b9564ec141614038ae0334a2
indaleen <inquinsayas@gmail.com>
2021-11-19 19:33:07 +0800

    Create sample2.cs

---
+++ sample2.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CSharpBasics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			string name = "Indaleen";
+			int age = 18;
+ 			Console.WriteLine("Hi " + name);
+ 			Console.WriteLine("Your age is " + age);
+        }
+    }
+}
