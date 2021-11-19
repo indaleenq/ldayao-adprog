8e2839bc2ef9db134410fabd5c9506680cc44e88
indaleen <inquinsayas@gmail.com>
2021-11-19 19:32:00 +0800

    Create sample1.cs

---
+++ sample1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpBasics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Micale Angel G. Becas");
+        }
+    }
+}
